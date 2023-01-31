7d554fd9d49ad2c94a739915c729008172e640bf
Golan3212 <93038099+Golan3212@users.noreply.github.com>
2023-01-31 19:43:44 +0600

    По заданию добавлено все, кроме рендера источников.

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -2,17 +2,17 @@
 @section('content')
     <div class="row mb-2">
 
-@forelse ($news as $n)
+@forelse ($newsList as $n)
             <div class="col-md-6">
                 <div class="card flex-md-row mb-4 box-shadow h-md-250">
                     <div class="card-body d-flex flex-column align-items-start">
-                        <strong class="d-inline-block mb-2 text-primary">{{$n['author']}}</strong>
+                        <strong class="d-inline-block mb-2 text-primary">{{$n->author}}</strong>
                         <h3 class="mb-0">
-                            <a class="text-dark" href="#">{{$n['title']}}</a>
+                            <a class="text-dark" href="#">{{$n->title}}</a>
                         </h3>
-                        <div class="mb-1 text-muted">{{$n['created_at']}}</div>
-                        <p class="card-text mb-auto">{{$n['description']}}</p>
-                        <a href="{{route('news.show', $n['id'])}}">Continue reading</a>
+                        <div class="mb-1 text-muted">{{$n->created_at}}</div>
+                        <p class="card-text mb-auto">{{$n->description}}</p>
+                        <a href="{{route('news.show', $n->id)}}">Continue reading</a>
                     </div>
                     <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
                 </div>
